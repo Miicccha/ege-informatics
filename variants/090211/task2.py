7c56febfb87dc31e1c182c96fc189d0c51af8895
Miicccha <miicccha@gmail.com>
2026-02-10 20:39:07 +0500

    11

--- variants/090211/task2.py
+++ variants/090211/task2.py
@@ -1,6 +1,13 @@
-n = (16**8*4**20-4**5-64)
-new_n=''
-while n>0:
-    new_n += str(n%4)
-    n = n//4
-print(new_n.count('3'))
+def R(N):
+    b = bin(N)[2:]
+    if N%3==0:
+        b = b+b[-3:]
+    else:
+        k =(N%3)*3
+        b=b+bin(k)[2:]
+    return int(b,2)
+
+for N in range(1,100):
+    if R(N) >= 76:
+        print(N, R(N))
+        break
