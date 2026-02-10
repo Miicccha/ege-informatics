0c5b93956c95c3ee6d2afdc2e4fb40ca896c1b3e
Miicccha <miicccha@gmail.com>
2026-02-10 20:00:38 +0500

    23334 реееея

--- variants/090211/task1.py
+++ variants/090211/task1.py
@@ -8,6 +8,6 @@ for w1 in aplh:
                     for w6 in aplh:
                         cnt +=1
                         W=w1+w2+w3+w4+w5+w6
-                        if w1 != 'е' and W.count('я') == 1 and cnt % 2 == 0:
+                        if (w1 != 'е' and w1 != 'и' and w1 != 'о' and W.count('я') == 1 and cnt % 2 == 0):
                             print(cnt, W) #7782
                             exit()
