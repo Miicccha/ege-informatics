4cead5521fa867156f3955500193be751c7e7314
Miicccha <miicccha@gmail.com>
2026-02-27 18:14:58 +0500

    нр

---
+++ variants/13hw/task1.py
@@ -0,0 +1,6 @@
+for x in range(0,2):
+    for y in range(0,2):
+        for z in range(0,2):
+            for w in range(0,2):
+                if((not x or not y) and(z or w)and z)==0:
+                    print(x,y,z,w)
