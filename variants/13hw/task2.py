da29b15bb6843bf7a45113fad2aece17042632a6
Miicccha <miicccha@gmail.com>
2026-02-27 18:15:04 +0500

    нр

---
+++ variants/13hw/task2.py
@@ -0,0 +1,14 @@
+alph='вулорф'
+cnt=0
+for s1 in alph:
+    for s2 in alph:
+        for s3 in alph:
+            for s4 in alph:
+                for s5 in alph:
+                    for s6 in alph:
+                        for s7 in alph:
+                            for s8 in alph:
+                                s=s1+s2+s3+s4+s5+s6+s7+s8
+                                cnt +=1
+                                if s.count('оверфлоу'):
+                                    print(s,cnt)
